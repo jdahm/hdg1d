803ff87f6cc299dc3f6c4cb6c8d7046360a4f4c5
Johann Dahm <johann.dahm@gmail.com>
2013-12-01 20:39:30 -0500

    Added trace error norm

--- error_norm.m
+++ error_norm.m
@@ -1,22 +1,26 @@
-function err = error_norm(Q, U, L, xnq, xnu, qorder, md, fd, norm_type, varargin)
+function err = error_norm(Q, U, L, xnq, xnu, qorder, md, fd, sd, norm_type, varargin)
 
   % perform a sanity check and pull off the anonymous functions in varargin
   switch norm_type
-    case {'L1', 'L2', 'Trace'}
+    case {'L1', 'L2', 'Trace', 'CellAvgU'}
       if length(varargin) < 1
-	error('need u(fd,x)');
+	error('need u(fd,sd,x)');
       end
       u = varargin{1};
+    case 'CellAvgQ'
+      if length(varargin) < 1
+	error('need u_x(fd,sd,x)');
+      end
+      u_x = varargin{1};
     case 'H1'
       if length(varargin) < 2
-	error('need u(fd,x), u_x(fd,x)');
+	error('need u(fd,sd,x), u_x(fd,sd,x)');
       end
       u = varargin{1};
       u_x = varargin{2};
     otherwise
       error('unknown norm type');
   end
-
 
   dx = (md.xe - md.xs) / md.ne;
 
@@ -51,30 +55,36 @@ function err = error_norm(Q, U, L, xnq, xnu, qorder, md, fd, norm_type, varargin
 	 uhra = uhre;
        end
     end
-    if strcmp(norm_type, 'L1') || strcmp(norm_type, 'L2') || strcmp(norm_type, 'H1')
+    if strcmp(norm_type, 'L1') || strcmp(norm_type, 'L2') || strcmp(norm_type, 'H1') || strcmp(norm_type, 'CellAvgU')
       UE = U((elem-1)*nnu+1:elem*nnu);
       ua = uPhi*UE;
-      ue = u(fd, xg);
+      ue = u(fd, sd, xg);
     end
-    if strcmp(norm_type, 'H1')
+    if strcmp(norm_type, 'H1') || strcmp(norm_type, 'CellAvgQ')
        QE = Q((elem-1)*nnq+1:elem*nnq);
        u_xa = qPhi*QE;
-       u_xe = u_x(fd, xg);
+       u_xe = u_x(fd, sd, xg);
     end
 
     % add to the norms
-    if strcmp(norm_type, 'Trace')
-      err = err + md.dx*((uhla - uhle)^2 + (uhra - uhre)^2);
-    elseif strcmp(norm_type, 'L1')
-      err = err + dx*sum(w.*abs(ua-ue));
-    elseif strcmp(norm_type, 'L2')
-      err = err + dx*sum(w.*(ua-ue).^2);
-    elseif strcmp(norm_type, 'H1')
-      erru = erru + dx*sum(w.*(ua-ue).^2);
-      errq = errq + dx*sum(w.*(u_xa-u_xe).^2);
-    else
-      error('unknown norm type');
+    switch norm_type
+      case 'Trace'
+	err = err + md.dx*((uhla - uhle)^2 + (uhra - uhre)^2);
+      case 'L1'
+	err = err + dx*sum(w.*abs(ua-ue));
+      case 'L2'
+	err = err + dx*sum(w.*(ua-ue).^2);
+      case 'H1'
+	erru = erru + dx*sum(w.*(ua-ue).^2);
+	errq = errq + dx*sum(w.*(u_xa-u_xe).^2);
+      case 'CellAvgU'
+	err = err + dx*abs(sum(w.*ua) - sum(w.*ue));
+      case 'CellAvgQ'
+	err = err + dx*abs(sum(w.*u_xa) - sum(w.*u_xe));
+      otherwise
+	error('unknown norm type');
     end
+
   end
 
   % post-summation operation
@@ -82,7 +92,4 @@ function err = error_norm(Q, U, L, xnq, xnu, qorder, md, fd, norm_type, varargin
     err = sqrt(err);
   elseif strcmp(norm_type, 'H1')
     err = sqrt(erru + errq);
-  elseif strcmp(norm_type, 'L1')
-  else
-    error('unknown norm type');
   end
